d369100d969a3beaf0848bea3cd9f74f22ed8ec8
Roman Bunyak <compcc@ya.ru>
2023-01-13 22:13:16 +0300

    add task 1.2.3.5.7

---
+++ Sem1_Tack1/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая будет выдавать
+// // название дня недели по заданному номеру
+Console.WriteLine("Введите число");
+// //считываем данные с консоли
+string? InputNum = Console.ReadLine();
+if (InputNum != null)
+// {
+//     // парсим введеное число
+//     int number = int.Parse(InputNum);
+//     // находим квадрат числа
+//     //int result = number * number;
+//     int result = (int)Math.Pow(number,2);
+    
+//     // выводим данные в консоль
+//     Console.WriteLine(result);
+// }
+
+Console.WriteLine(Math.Pow(int.Parse(InputNum, 2)));
