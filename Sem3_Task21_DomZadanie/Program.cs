5cc79abc1b888b6f1f5cab0b192026993db792a4
Roman Bunyak <compcc@ya.ru>
2023-01-24 19:51:46 +0300

    add WorkHome Task 19, 21, 23

---
+++ Sem3_Task21_DomZadanie/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// * Сделать метод загрузки точек
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Выводим результат пользователю
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+
+//Вычисляем расстояние м-у точками
+double CalcLen(int x1, int x2, int y1, int y2, int z1, int z2)
+{// объявляем переменную куда будем записывать результат
+    double res = 0;
+    //процесс вычисления
+    res = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)+ Math.Pow(z1 - z2, 2));
+    //вывод результата из метода
+    return res;
+}
+
+//вводим координаты точек
+int coorX1 = ReadData("Введие координату Х1");
+int coorX2 = ReadData("Введие координату Х2");
+int coorY1 = ReadData("Введие координату Y1");
+int coorY2 = ReadData("Введие координату Y2");
+int coorZ1 = ReadData("Введие координату Z1");
+int coorZ2 = ReadData("Введие координату Z2");
+
+//вычисляем длинну с помощю метода CalcLen
+double len = CalcLen(coorX1, coorX2, coorY1, coorY2, coorZ1, coorZ2);
+PrintData("Расстояние м-у точками:", len);
---
+++ Sem3_Task23_DomZadanie/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// * Вывести таблицу с границами и значениями друг над другом
+// Читаем данные пользоваеля
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//с помощю метода печатаем результат
+void PrintData(string up, string down)
+{
+    Console.WriteLine(up);
+    Console.WriteLine(down);
+}
+//метод построения 2х строчек результата
+string LineBuilder(int n, int p)
+{
+    //объявляем пустую строку куда будем накапливать результаты работы цикла for
+    string s = "";
+    for (int i = 1; i <= n; i++)
+    {
+        //вычисляем степень от 1 до введеного пользователем числа n
+        s += Math.Pow(i, p).ToString() + "\t";
+    }
+    return s;
+}
+
+int n = ReadData("Введите чило N: ");
+//вводим данные в метод LineBuilder, сначала первой сточки
+string up = LineBuilder(n, 1);
+//затем второй в кубе
+string down = LineBuilder(n, 3);
+//выводим результат на консоль
+PrintData(up, down);
