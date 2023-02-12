16067f0e92f0ce2e2bda9db5cc0f1b1f9d9a69e5
Roman Bunyak <compcc@ya.ru>
2023-02-12 10:44:19 +0300

    add solution home work task 64, 66, 68

---
+++ Sem9_Task66_DomZadanie/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+int RecSumMN(int m, int n)
+{
+    if (m >= n) return n; // окончание рекурсии происходит когда минимальное число будет равно максимальномоу
+    return m + RecSumMN(m + 1, n); //иначе повторно вызываем метод RecSumMN и прибавляем к минимальному значению 1
+}
+
+
+int m = ReadData("Введите значение числа M:");
+int n = ReadData("Введите значение числа N:");
+int res = m < n ? RecSumMN(m, n): RecSumMN(n, m);
+PrintResult(res);
---
+++ Sem9_Task68_DomZadanie/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+
+void PrintResult(int prefix)
+{
+    Console.Write(prefix);
+}
+
+int FAccerman(int m, int n) //метод вычисления функции Аккермана
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return FAccerman(m - 1, 1);
+    if (m > 0 && n > 0) return FAccerman(m - 1, FAccerman(m, n - 1));
+    return FAccerman(m, n);
+}
+
+int m = ReadData("Введите значение числа M:");
+int n = ReadData("Введите значение числа N:");
+int a = FAccerman(m, n);
+PrintResult(a);
